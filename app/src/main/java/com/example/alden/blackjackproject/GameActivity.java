10967c8ae2e9beddb2fe62d1dacce691fca08359
Alden Poole <aldenpoole1@users.noreply.github.com>
2018-11-30 09:49:06 -0500

    added button functionality
    all bet buttons update bet amount and bank amount, set text views to represent amount

--- app/src/main/java/com/example/alden/blackjackproject/GameActivity.java
+++ app/src/main/java/com/example/alden/blackjackproject/GameActivity.java
@@ -1,15 +1,64 @@
 package com.example.alden.blackjackproject;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.Random;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
+import android.content.Context;
 
 public class GameActivity extends AppCompatActivity {
+
+    private int bank;
+    private int bet;
+    TextView bankText, betText;
+    Button bet100,bet500,bet50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
+        bank = 1000;
+        bet = 0;
+
+        bankText=(TextView)findViewById(R.id.bank_total);
+        betText=(TextView)findViewById(R.id.bet_total);
+
+        bankText.setText("Bank Total: $" + bank);
+        betText.setText("Bet Total: $" + bet);
+
+        bet100 = findViewById(R.id.button_hundred);
+        bet50 = findViewById(R.id.button_fifty);
+        bet500 = findViewById(R.id.button_five);
+
+        bet100.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                bet100();
+            }
+        });//Close open game button
+
+        bet500.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                bet500();
+            }
+        });//Close open game button
+
+        bet50.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                bet50();
+            }
+        });//Close open game button
+
     }
 
     public void resetGame(){
@@ -21,8 +70,52 @@ public class GameActivity extends AppCompatActivity {
     }
 
     public void bet100(){
+        if(bank >= 100){
+            bank-=100;
+            bet+=100;
+            setTexts();
+        }else{
+            Context context = getApplicationContext();
+            CharSequence text = "Not enough funds!";
+            int duration = Toast.LENGTH_SHORT;
 
-
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
 
+    public void bet500(){
+        if(bank >= 500){
+            bank-=500;
+            bet+=500;
+            setTexts();
+        }else{
+            Context context = getApplicationContext();
+            CharSequence text = "Not enough funds!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
+
+    public void bet50(){
+        if(bank >= 50){
+            bank-=50;
+            bet+=50;
+            setTexts();
+        }else{
+            Context context = getApplicationContext();
+            CharSequence text = "Not enough funds!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
+
+    public void setTexts(){
+        bankText.setText("Bank Total: $" + bank);
+        betText.setText("Bet Total: $" + bet);
+    }
 }
