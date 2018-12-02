3125d4279dc6da808b68d12c2e88de35b609d8c2
Alden Poole <aldenpoole1@users.noreply.github.com>
2018-12-02 13:06:24 -0500

    buttons update
    buttons that navigate to about and settings page are now functional.

---
+++ app/src/main/java/com/example/alden/blackjackproject/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.example.alden.blackjackproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class AboutActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_me_activity);
+
+
+    }
+}
--- app/src/main/java/com/example/alden/blackjackproject/MainActivity.java
+++ app/src/main/java/com/example/alden/blackjackproject/MainActivity.java
@@ -8,25 +8,47 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button openGame;
+    Button openGame, openSettings, openAbout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         openGame = findViewById(R.id.start_game_button);
+        openSettings = findViewById(R.id.settings_button);
+        openAbout = findViewById(R.id.about_button);
 
         openGame.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                openCraps();
+                openGame();
+            }
+        });//Close open game button
+        openSettings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openSettings();
+            }
+        });//Close open game button
+        openAbout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openAbout();
             }
         });//Close open game button
 
     }
 
-    public void openCraps(){
+    public void openGame(){
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
-    } //Close openCraps
+    } //Close openGame
+    public void openSettings(){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    } //Close openSettings
+    public void openAbout(){
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+    } //Close openAbout
 }
